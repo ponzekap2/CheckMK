043694c78c4d74d654e67a739277d1d34a235ab7
Paul Ponzeka <ponzekap2@gmail.com>
2022-04-14 05:43:14 -0400

    Initial Upload

---
+++ Check-OSPFAlerts.ps1
@@ -0,0 +1,5 @@
+$requestBody = 'request={"hostname":"' + "TXDC-SOADC01" + '","folder":"TexasDatacenter","attributes":{"ipaddress":"' + "10.220.48.220" + '","site":"texas","tag_agent":"cmk-agent"}}'
+Invoke-WebRequest "http://txmgmtcmk01/texas/check_mk/webapi.py?action=add_host&_username=automation&_secret=MSSS@@GBHRVVPXD@KFTA" -Method Post -Body $requestBody
+
+
+
